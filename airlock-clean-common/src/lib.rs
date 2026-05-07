9afbc483941eb650a0b34f05f717273c29292a54
wazulu <wazulu@local>
2026-05-07 18:30:29 +0530

    phase4: introduce shared FileIdentity ABI crate

--- airlock-clean-common/src/lib.rs
+++ airlock-clean-common/src/lib.rs
@@ -1,1 +1,14 @@
 #![no_std]
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct FileIdentity {
+    pub dev: u64,
+    pub ino: u64,
+}
+
+// Compile-time ABI guard
+const _: [(); 16] = [(); core::mem::size_of::<FileIdentity>()];
+
+pub const ACTION_ALLOW: u32 = 0;
+pub const ACTION_DENY: u32 = 1;
